fe67046e40d7177dbba1342ebd898dae7d1f5221
AnnaSam48 <anna.samborska@accenture.com>
2020-05-07 15:06:52 +0300

    application.properties file updated with h2 database configuration and WebConfiguration class added

---
+++ src/main/java/com/accenture/weatherForecastWebsite/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.accenture.weatherForecastWebsite;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/img/**",
+                "/css/**",
+                "/js/**")
+                .addResourceLocations(
+                        "classpath:/static/img/",
+                        "classpath:/static/css/",
+                        "classpath:/static/js/");
+    }
+
+}
