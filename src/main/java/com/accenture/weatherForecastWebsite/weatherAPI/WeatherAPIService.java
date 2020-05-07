5e98a7cba92143dd00c4c3bf7047c6c0b7a95213
krista-tech <kristabokanova@gmail.com>
2020-05-07 12:03:13 +0300

    Api Service Reader JsonResponse toString. Not finished

--- src/main/java/com/accenture/weatherForecastWebsite/weatherAPI/WeatherAPIService.java
+++ src/main/java/com/accenture/weatherForecastWebsite/weatherAPI/WeatherAPIService.java
@@ -3,6 +3,12 @@ package com.accenture.weatherForecastWebsite.weatherAPI;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Component
 public class WeatherAPIService {
     @Value("${weather.api.request}")
@@ -13,5 +19,28 @@ public class WeatherAPIService {
 
     @Value("${weather.api.key}")
     private String apiKey;
+
+    private String getJsonResponse(URL url) throws Exception {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setReadTimeout(3000);
+        urlConnection.connect();
+
+        InputStream inputStream = urlConnection.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        String line = null;
+
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line + "\n");
+        }
+        String JsonResponse = stringBuilder.toString();
+        bufferedReader.close();
+
+        return JsonResponse;
+    }
+
 
 }
