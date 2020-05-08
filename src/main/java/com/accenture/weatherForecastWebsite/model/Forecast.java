6961f5dad6775cd43585f1c84d2b8f15d5c0bdb5
krista-tech <kristabokanova@gmail.com>
2020-05-08 15:50:00 +0300

    Added get/set for test parts while json is coming up. new adds can be deleted, but please change information in controllers regarding to this all.

--- src/main/java/com/accenture/weatherForecastWebsite/model/Forecast.java
+++ src/main/java/com/accenture/weatherForecastWebsite/model/Forecast.java
@@ -11,6 +11,35 @@ public class Forecast implements Serializable {
     @Column (length = 12)
     private Long id;
     private String name;
+    private int temperature; //delete afterwards, for test purposes while we have no data
 
+    public Forecast(Long id, String name, int temperature) {
+        this.id = id;
+        this.name = name;
+        this.temperature = temperature;
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
 }
