096359c6dbe1aa32f4f9b886254fa16347581224
krista-tech <kristabokanova@gmail.com>
2020-05-13 14:31:27 +0300

    unuseful parts deleted

---
+++ src/main/java/com/accenture/weatherForecastWebsite/newVersion/WebConfiguration.java
@@ -0,0 +1,13 @@
+package com.accenture.weatherForecastWebsite.newVersion;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfiguration {
+
+
+}
