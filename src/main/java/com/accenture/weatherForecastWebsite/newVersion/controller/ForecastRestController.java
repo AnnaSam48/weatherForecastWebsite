dbd5e2fd384848aac0e943fb772d8de09e2b379a
krista-tech <kristabokanova@gmail.com>
2020-05-13 20:35:34 +0300

    logic about when to make api request and what to update, set in logic. if new city: api request if update happened in last h, return info from database if info older update needed parts

--- src/main/java/com/accenture/weatherForecastWebsite/newVersion/controller/ForecastRestController.java
+++ src/main/java/com/accenture/weatherForecastWebsite/newVersion/controller/ForecastRestController.java
@@ -1,9 +1,13 @@
 package com.accenture.weatherForecastWebsite.newVersion.controller;
+
 import com.accenture.weatherForecastWebsite.newVersion.model.Cities;
 import com.accenture.weatherForecastWebsite.newVersion.repository.ForecastsByCityRepository;
 import com.accenture.weatherForecastWebsite.newVersion.weatherAPI.WeatherAPIService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
 
 @RestController
 @RequestMapping("forecast")
@@ -28,6 +32,9 @@ public class ForecastRestController {
     @PostMapping(value = "/{cityName}")
     public Cities setForecast(@PathVariable String cityName) {
         Cities matchedLocation = forecastsByCityRepository.findByCityName(cityName);
+        long currentTimeRaw = System.currentTimeMillis();
+        Timestamp currentTime = new Timestamp(currentTimeRaw);
+
         if (matchedLocation == null) {
             Cities getNewCity = weatherAPIService.getForecastByCity(cityName);
             Cities cityToAdd = new Cities();
@@ -37,10 +44,40 @@ public class ForecastRestController {
             cityToAdd.setTemp(getNewCity.getTemp());
             cityToAdd.setSunrise(getNewCity.getSunrise());
             cityToAdd.setSunset(getNewCity.getSunset());
+            cityToAdd.setTimestamp(currentTime);
             forecastsByCityRepository.save(cityToAdd);
             System.out.println("new city added");
             return cityToAdd;
         } else {
+            String matchedLocationId = matchedLocation.getId();
+            long lastUpdateTime = matchedLocation.getTimestamp();
+            long currentTimeMinusH = System.currentTimeMillis() - (60 * 60 * 1000);
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            String currentDate=simpleDateFormat.format(currentTime);
+            String lastUpdateDate=simpleDateFormat.format(lastUpdateTime);
+
+            if (lastUpdateTime < currentTimeMinusH || lastUpdateTime == currentTimeMinusH) {
+
+                Cities cityForUpdate = weatherAPIService.getForecastByCityID(matchedLocationId);
+                matchedLocation.setTimestamp(currentTime);
+                //API updates information in their db in every 2h, as we don't know at what time, we update every 1h, to get more precise data
+
+                if (currentDate!=lastUpdateDate) {
+                    //last update didn't happen today
+                    matchedLocation.setSunrise(cityForUpdate.getSunrise());
+                    matchedLocation.setSunset(cityForUpdate.getSunset());
+                    matchedLocation.setTemp(cityForUpdate.getTemp());
+                }else {//last update is older than 1 h
+                    matchedLocation.setTemp(cityForUpdate.getTemp());
+                    System.out.println("city was updated today, but not in last hour");
+                }
+                forecastsByCityRepository.save(matchedLocation);
+            } else {
+                System.out.println("city is already updated in time of last h");
+                return matchedLocation;
+            }
+
             System.out.println("city is already in DB");
             return matchedLocation;
         }
@@ -53,27 +90,20 @@ public class ForecastRestController {
 
       /*
             //TODO
-            // make timestamp, to each table update for field
-            // if(time.stamp is not older than 2h (API Documentation) then return DB info
-            // else (ask for api for new information in needed parts)
-            // latter separate dB table in normalisation levels
-            // ID + name + country + lon/lat +timezone(probably needed for sunset)
-            // sunrise + sunset table updates not more than once in day \
+            // separate dB table in normalisation levels
+            // ID + name + country + lon/lat
+            // sunrise + sunset table updates not more than once in day
             // temp and timestamp should be updated every two h + could add wind/clouds
 
-            TODO if not updated since 00:00 at the place
-            Cities cityUpdate = weatherAPIService.getForecastByCity(cityName);
-            matchedLocation.setSunset(cityUpdate.getSunset());
-            matchedLocation.setSunrise(cityUpdate.getSunrise());
-            //++ all the rest fields that are updatable
-            matchedLocation.setTemp(cityUpdate.getTemp());
-            + Update time stamp with new
+              */
+ /*
+            //TODO
+            // Make classes for conversions date/Kelvin and all the rest ones
+              */
 
-            TODO if update has happened today, but haven't been updated last two h
-            Cities cityUpdate = weatherAPIService.getForecastByCity(cityName);
-            matchedLocation.setTemp(cityUpdate.getTemp());
-            + Update time stamp with new
-
+ /*
+            //TODO
+            // Make test classes
               */
 
 
