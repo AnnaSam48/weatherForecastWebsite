ef2272b8ed64a2898bc90c5fad25bb0cb1f774a5
krista-tech <kristabokanova@gmail.com>
2020-05-21 08:00:53 +0300

    example

---
+++ src/test/java/com/accenture/weatherForecastWebsite/newVersion/controller/CityTest.java
@@ -0,0 +1,26 @@
+/*package com.accenture.weatherForecastWebsite.newVersion.controller;
+
+import com.accenture.weatherForecastWebsite.version2.model.City;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+
+import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
+
+public class CityTest {
+    @Test
+    public void whenUsingAnnotations_thenOk() throws IOException {
+      City city = new ObjectMapper()
+                .readerFor(City.class)
+                .readValue(SOURCE_JSON);
+
+
+        assertEquals(product.getName(), "The Best Product");
+        assertEquals(product.getBrandName(), "ACME Products");
+        assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
+    }
+}
+
+ */
