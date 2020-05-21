3cff5280ba76a88b8997ea8b76679c49b956a1b5
krista-tech <kristabokanova@gmail.com>
2020-05-21 08:31:12 +0300

    city repository

---
+++ src/main/java/com/accenture/weatherForecastWebsite/version2/repository/ForecastsByCityRepository.java
@@ -0,0 +1,14 @@
+package com.accenture.weatherForecastWebsite.version2.repository;
+
+
+import com.accenture.weatherForecastWebsite.version2.model.City;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ForecastsByCityRepository extends CrudRepository<City, String> {
+    Optional<City> findById(String id);
+    City findByCityName(String cityName);
+}
