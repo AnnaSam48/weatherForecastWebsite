94c70d6c518bff50f8de783474fcf9732fb2f519
krista-tech <kristabokanova@gmail.com>
2020-05-22 09:17:38 +0300

    update for search in db by name Needs test

--- src/main/java/com/accenture/weatherForecastWebsite/version2/repository/ForecastsByCityRepository.java
+++ src/main/java/com/accenture/weatherForecastWebsite/version2/repository/ForecastsByCityRepository.java
@@ -3,12 +3,14 @@ package com.accenture.weatherForecastWebsite.version2.repository;
 
 import com.accenture.weatherForecastWebsite.version2.model.City;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.util.Streamable;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface ForecastsByCityRepository extends CrudRepository<City, String> {
+
     Optional<City> findById(String id);
-    City findByCityName(String cityName);
+    Streamable<City> findByCityName(String cityName);
 }
