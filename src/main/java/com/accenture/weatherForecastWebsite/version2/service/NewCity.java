65d7dfbe5f89fc9771ec2b1c78414fad0e450409
krista-tech <kristabokanova@gmail.com>
2020-05-25 10:08:20 +0300

    class, for setting new city, that data base doesn't contain jet

---
+++ src/main/java/com/accenture/weatherForecastWebsite/version2/service/NewCity.java
@@ -0,0 +1,41 @@
+package com.accenture.weatherForecastWebsite.version2.service;
+
+
+import com.accenture.weatherForecastWebsite.version2.ApiService.WeatherAPIService;
+import com.accenture.weatherForecastWebsite.version2.controller.ForecastRestController;
+import com.accenture.weatherForecastWebsite.version2.model.City;
+import com.accenture.weatherForecastWebsite.version2.repository.ForecastsByCityRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+
+@Component
+public class NewCity {
+    @Autowired
+    WeatherAPIService weatherAPIService;
+    @Autowired
+    ForecastsByCityRepository forecastsByCityRepository;
+
+    Logger serviceLogger = LoggerFactory.getLogger(ForecastRestController.class);
+
+    public City setCityToAdd(String cityToAdd) {
+
+        City getNewCity = weatherAPIService.getForecastByCity(cityToAdd);
+        City newCity = new City();
+        newCity.setId(getNewCity.getId());
+        newCity.setCityName(getNewCity.getCityName());
+        newCity.setCountry(getNewCity.getCountry());
+        newCity.setTemperature(getNewCity.getTemperature());
+        newCity.setSunrise(getNewCity.getSunrise());
+        newCity.setSunset(getNewCity.getSunset());
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+        newCity.setTimestamp(currentTime);
+        forecastsByCityRepository.save(newCity);
+
+        serviceLogger.trace(getNewCity.getCityName() + " added in database...");
+        return newCity;
+    }
+}
